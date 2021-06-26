c729540ae5c23408e7b144ecf070167b7a397e33
palani-08 <palani7pavan@gmail.com>
2021-06-26 20:07:31 +0530

    some changes made

--- app.js
+++ app.js
@@ -20,8 +20,26 @@ const vue = new Vue({
         .then(response => response.json()).then(data =>{
             this.stateList = data.response;
         });
-    }
+    },
+ getData (){
+        fetch("https://covid-19-india-data-by-zt.p.rapidapi.com/GetIndiaStateWiseData" + this.state, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "6aa64bde4cmsh93846a66bb93317p172b0djsn763bdaf83e25",
+                "x-rapidapi-host": "covid-19-india-data-by-zt.p.rapidapi.com"
+            }
+        })
+        .then(response => response.json()).then(data =>{
+            data = data.response[0];
+            this.cases = data.cases.total;
+            this.death = data.death.total;
+            this.recovered = data.cases.recovered;
+        
+         
+        });
+     }
 },
+
             
        mounted() {
            this.getState();
