573b28885260e16485303b8b7dbdf66a4c8166c6
palani-08 <palani7pavan@gmail.com>
2021-06-28 12:15:11 +0530

    tried using axios

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+
 const vue = new Vue({
     el: '#app' ,
     data: {
-        state:'',
-        stateList: '',
+        state: '',
+        statelist: '',
         cases: '',
         death: '',
         recovered: ''
@@ -10,41 +11,27 @@ const vue = new Vue({
     },
     methods: {
        getState() {
-        fetch("https://covid19india.p.rapidapi.com/getStateData", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "6aa64bde4cmsh93846a66bb93317p172b0djsn763bdaf83e25",
-                "x-rapidapi-host": "covid19india.p.rapidapi.com"
-            }
-        })
-        .then(response => response.json()).then(data =>{
-            this.stateList = data.response;
-        });
-    },
- getData (){
-        fetch("https://covid-19-india-data-by-zt.p.rapidapi.com/GetIndiaStateWiseData" + this.state, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "6aa64bde4cmsh93846a66bb93317p172b0djsn763bdaf83e25",
-                "x-rapidapi-host": "covid-19-india-data-by-zt.p.rapidapi.com"
-            }
-        })
-        .then(response => response.json()).then(data =>{
-            data = data.response[0];
-            this.cases = data.cases.total;
-            this.death = data.death.total;
-            this.recovered = data.cases.recovered;
-        
-         
-        });
-     }
-},
+        var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'https://covidtracking.com/api/states',
+  headers: { }
+};
+import axios from 'axios'
+axios(config)
+.then((res) => res.json())
+.then(data => this.states = data.response)
+.catch(err => console.log(err.message))
+  },
+  
+ 
 
             
        mounted() {
            this.getState();
        }
     
-
+      }
 
 });
