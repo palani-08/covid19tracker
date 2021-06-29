70b69a658956e6233ef55348b08eeab498e98986
palani-08 <palani7pavan@gmail.com>
2021-06-29 15:47:26 +0530

    completed

--- app.js
+++ app.js
@@ -1,64 +1,59 @@
 
 const vue = new Vue({
-    el: '#app' ,
-    data: {
-        country: '',
-        countryList: [],
-        cases: '',
-        death: '',
-        recovered: ''
+   el: '#app' ,
+    data:{
+      country: '',
+      countryList: [],
+      cases: '',
+      death:'',
+      recovered:''
 
-    },
-    
-    methods: {
-      
-       getCountry() {
-         import axios from 'axios';
-         import Vueaxios from 'vueaxios';
-        var axios = require('axios');
-
-        var config = {
-          method: 'get',
-          url: 'https://corona.lmao.ninja/v2/countries?yesterday&sort',
-          headers: { }
-        };
-axios(config)
-.then(response => response.json()).then(data =>{
-  console.log( this.countryList =  data.country);
-})
-.catch(function (error) {
-  console.log(error);
-});
-        
-      },
-
-      getdata(){
-        var axios = require('axios');
-
-        var config = {
-          method: 'get',
-          url: 'https://corona.lmao.ninja/v2/countries?yesterday&sort' + this.country,
-          headers: { }
-        };
-axios(config)
-.then(response => response.json()).then(data =>{
-  data =  data.response[0];
-  this.cases = data.cases
-  this.death = data.deaths
-  this.recovered = data.recovered
-});   
-
-      }
   },
-  
- 
-
-            
-       mounted() {
-          this.getCountry();
-        
-       },
-    
-     
-
+  methods: {
+    getCountry() {
+      fetch("https://covid-193.p.rapidapi.com/countries", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "6aa64bde4cmsh93846a66bb93317p172b0djsn763bdaf83e25",
+		"x-rapidapi-host": "covid-193.p.rapidapi.com"
+	}
+}).then(response => response.json()).then(data =>{
+  this.countryList = data.response;
 });
+     
+   },
+
+   getdata(){
+    fetch("https://covid-193.p.rapidapi.com/statistics?country=" + this.country, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "6aa64bde4cmsh93846a66bb93317p172b0djsn763bdaf83e25",
+		"x-rapidapi-host": "covid-193.p.rapidapi.com"
+	}
+}).then(response => response.json()).then(data =>{
+  data = data.response[0];
+  this.cases = data.cases.total;
+  this.death = data.deaths.total;
+  this.recovered = data.cases.recovered;
+
+   });
+  }
+},
+
+
+
+         
+    mounted() {
+       this.getCountry();
+     
+    },
+ 
+  
+  });
+
+
+  
+  
+    
+    
+  
